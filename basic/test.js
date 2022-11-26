ed5fdcf4ebdc71ebd09dcbcc95f59fc2189c4b2f
Ilia Kirichek <iokirichek@protonmail.com>
2022-11-26 05:09:59 +0400

    Use wallet with in-page-provider

--- basic/test.js
+++ basic/test.js
@@ -25,106 +25,101 @@ const ever = new ProviderRpcClient({
     }),
 });
 
-
 async function main() {
-  try {
-    // Add giver keypair to provider keystore
-    // Giver is a generic name for any smart contract
-    // that is used to send Ever's for deploying
-    // smart contracts by an external message.
+  // Add giver keypair to provider keystore
+  // Giver is a generic name for any smart contract
+  // that is used to send Ever's for deploying
+  // smart contracts by an external message.
 
-    // The local node has a pre-deployed giver
-    // with a simple interface. Add their keypair
-    // to the keyStore, because provider
-    // always looking into keystore to find a keypair
-    // to sign an external message.
-    keyStore.addKeyPair(getGiverKeypair());
+  // The local node has a pre-deployed giver
+  // with a simple interface. Add their keypair
+  // to the keyStore, because provider
+  // always looking into keystore to find a keypair
+  // to sign an external message.
+  keyStore.addKeyPair(getGiverKeypair());
 
-    // Generate a random keypair to set as owner of
-    // our SimpleStorage contract. In production, you
-    // of course need to import this keypair.
-    const keyPair = SimpleKeystore.generateKeyPair();
-    keyStore.addKeyPair(keyPair);
+  // Generate a random keypair to set as owner of
+  // our SimpleStorage contract. In production, you
+  // of course need to import this keypair.
+  const keyPair = SimpleKeystore.generateKeyPair();
+  keyStore.addKeyPair(keyPair);
 
 
-    // Calculate a stateInit of our contract.
+  // Calculate a stateInit of our contract.
 
-    // StateInit - it is a packed contract Code and InitialData(storage)
+  // StateInit - it is a packed contract Code and InitialData(storage)
 
-    // If you need to deploy a contract you need to attach
-    // StateInit to the message (Internal or External)
-    // If the target contract is not deployed before the transaction
-    // starts validator will check is hash(stateInit) == address
-    // of the destination contract.
+  // If you need to deploy a contract you need to attach
+  // StateInit to the message (Internal or External)
+  // If the target contract is not deployed before the transaction
+  // starts validator will check is hash(stateInit) == address
+  // of the destination contract.
 
-    // If they match validator will initialize contract by the
-    // given code + initialData from the stateInit.
+  // If they match validator will initialize contract by the
+  // given code + initialData from the stateInit.
 
-    // We also got expectedAddress, because address is just hash(stateInit)
-    const {address: expectedAddress, stateInit} = await ever.getStateInit(SimpleStorageContract.abi, {
-      // Tvc it is code + zeroInitial data.
-      // function will replace zero data to actual
-      // from initParams.
-      tvc: SimpleStorageContract.tvc,
-      workchain: 0,
-      publicKey: keyPair.publicKey,
-      initParams: {
-        random_number: Math.floor(Math.random() * 10000)
-      }
-    });
-
-    // So to deploy smart-contract by the external message we need to
-    // send some tokens with bounce: false flag to their address first.
-    // After it account state will change their status
-    // from NotExist to NotInitialized.
-    // Local node has pre-deployed giver. For the testnet/mainnet
-    // You need to setup giver by yourself.
-    // Check "Setup environment for testnet/mainnet" article
-    // to figure out how to do this.
-    await getTokensFromGiver(ever, expectedAddress, 1_000_000_000); // 1 ever = 1_000_000_000 nanoEvers
-
-    // Now we can send an external message to our account
-    const contract = new ever.Contract(SimpleStorageContract.abi, expectedAddress);
-
-    // We just send an external message to our contract
-    // that one will call constructor with arguments _initial_value = 1
-    // And also we attach stateInit to our message,
-    // because our contract must be initialized first.
-
-    await extractError(contract.methods.constructor({_initial_value: 1}).sendExternal({
-      stateInit: stateInit,
-      // Provider will search for the signer for this pubkey in the keyStore
-      publicKey: keyPair.publicKey,
-    }))
-
-    // Call view method 'get'.
-    // To do this, sdk will download full account state and run TVM locally
-    let {value0: variableValue} = await contract.methods.get({}).call({});
-    console.log('Account successfully deployed at address', expectedAddress.toString(), 'variable is set to', variableValue);
-
-    console.log('Set variable to 42')
-    await extractError(contract.methods.set({_value: 42}).sendExternal({
-      publicKey: keyPair.publicKey,
-    }))
-
-    let {value0: newVariableValue} = await contract.methods.get({}).call({});
-
-    if (newVariableValue !== '42') {
-      throw new Error('Variable is not 42')
-    } else {
-      console.log('Success, variable value is', newVariableValue);
+  // We also got expectedAddress, because address is just hash(stateInit)
+  const {address: expectedAddress, stateInit} = await ever.getStateInit(SimpleStorageContract.abi, {
+    // Tvc it is code + zeroInitial data.
+    // function will replace zero data to actual
+    // from initParams.
+    tvc: SimpleStorageContract.tvc,
+    workchain: 0,
+    publicKey: keyPair.publicKey,
+    initParams: {
+      random_number: Math.floor(Math.random() * 10000)
     }
+  });
 
-    console.log("Test successful");
-  } catch (e) {
-    console.error(e);
+  // So to deploy smart-contract by the external message we need to
+  // send some tokens with bounce: false flag to their address first.
+  // After it account state will change their status
+  // from NotExist to NotInitialized.
+  // Local node has pre-deployed giver. For the testnet/mainnet
+  // You need to setup giver by yourself.
+  // Check "Setup environment for testnet/mainnet" article
+  // to figure out how to do this.
+  await getTokensFromGiver(ever, expectedAddress, 1_000_000_000); // 1 ever = 1_000_000_000 nanoEvers
+
+  // Now we can send an external message to our account
+  const contract = new ever.Contract(SimpleStorageContract.abi, expectedAddress);
+
+  // We just send an external message to our contract
+  // that one will call constructor with arguments _initial_value = 1
+  // And also we attach stateInit to our message,
+  // because our contract must be initialized first.
+
+  await extractError(contract.methods.constructor({_initial_value: 1}).sendExternal({
+    stateInit: stateInit,
+    // Provider will search for the signer for this pubkey in the keyStore
+    publicKey: keyPair.publicKey,
+  }))
+
+  // Call view method 'get'.
+  // To do this, sdk will download full account state and run TVM locally
+  let {value0: variableValue} = await contract.methods.get({}).call({});
+  console.log('Account successfully deployed at address', expectedAddress.toString(), 'variable is set to', variableValue);
+
+  console.log('Set variable to 42')
+  await extractError(contract.methods.set({_value: 42}).sendExternal({
+    publicKey: keyPair.publicKey,
+  }))
+
+  let {value0: newVariableValue} = await contract.methods.get({}).call({});
+
+  if (newVariableValue !== '42') {
+    throw new Error('Variable is not 42')
+  } else {
+    console.log('Success, variable value is', newVariableValue);
   }
+
+  console.log("Test successful");
 }
 
 async function extractError(transactionPromise) {
   return transactionPromise.then(res => {
     if (res.transaction.aborted) {
-      throw new Error(`Transaction aborted with code ${transaction.exitCode}`)
+      throw new Error(`Transaction aborted with code ${res.transaction.exitCode}`)
     }
     return res;
   });
--- use_wallet/test.js
+++ use_wallet/test.js
@@ -1,245 +1,253 @@
-const { Account } = require("@eversdk/appkit");
-const {
-  TonClient,
-  signerKeys,
-  signerNone,
-} = require("@eversdk/core");
-const { libNode } = require("@eversdk/lib-node");
+const { Address, ProviderRpcClient } = require('everscale-inpage-provider');
+const { EverscaleStandaloneClient, EverWalletAccount, SimpleAccountsStorage } = require('everscale-standalone-client/nodejs');
+const { SimpleKeystore } = require("everscale-standalone-client/client/keystore");
+const { getGiverKeypair, getTokensFromGiver } = require("./giver");
 const BigNumber = require('bignumber.js');
 
-TonClient.useBinaryLibrary(libNode);
+const { DiceContract } = require('./artifacts/DiceContract');
 
-const { DiceContract } = require("./artifacts/DiceContract");
-const { SetcodeMultisigContract } = require("./artifacts/SetcodeMultisigContract");
+const keyStore = new SimpleKeystore();
+const accountStorage = new SimpleAccountsStorage();
 
-// We create a client connection to the local node
-const client = new TonClient({
-  network: {
-    // Local EVER OS SE instance URL here
-    endpoints: [ "http://localhost" ]
-  }
-});
-
-async function main(client) {
-  try {
-    // generate random keys pairs
-    const players_keys = await TonClient.default.crypto.generate_random_sign_keys();
-    const casino_owner_keys = await TonClient.default.crypto.generate_random_sign_keys();
-
-    // create an instance of multisig for the casino owner
-    let casinoOwnerMultisig = new Account(SetcodeMultisigContract, {
-      signer: signerKeys(casino_owner_keys),
-      client,
-      initData: {},
-    });
-
-    const casinoOwnerMultisigAddress = await casinoOwnerMultisig.getAddress();
-
-    // Deploy msig with 1 owner for casino owner
-    await casinoOwnerMultisig.deploy({
-      useGiver: true,
-      initInput: {
-        //constructor values
-        owners: [`0x${casino_owner_keys.public}`],
-        reqConfirms: 1,
-        lifetime: 3600
-      }})
-
-    console.log(`Casino owner multisig is deployed : ${casinoOwnerMultisigAddress}`);
-
-    // Our dice conrract instance
-    let diceContract = new Account(DiceContract,{
-      signer: signerNone(),
-      client,
-      initData: {
-        owner_: casinoOwnerMultisigAddress
-      },
-    });
-
-    // To deploy the contract from another contract we need to calculate initial data (static variables + pubkey)
-    let initData = (await client.abi.encode_initial_data({
-      abi: diceContract.abi,
-      initial_data: {
-        owner_: casinoOwnerMultisigAddress
-      },
-      initial_pubkey: `0x0000000000000000000000000000000000000000000000000000000000000000` // if zero pubkey must be such string
-    })).data
-
-    // We take code + initial data and make StateInit.
-    // We will send stateInit along with the message to call the constructor.
-    // Validator will check is hash(stateInit) === address and will Initialize our contract
-    // (will add code + initial data to account )
-    const diceContractStateInit = (await client.boc.encode_tvc({
-      code: DiceContract.code,
-      data: initData
-    })).tvc;
-
-    // Secondly we encode a message witch one will call the constructor in the same transaction
-    // Technically contract can be deployed without calling a constructor or any other function and the account will have
-    // status - active. Solidity has a special hidden variable "_constructorFlag" and always check it before any call.
-    // So your contract can not be called before the constructor will be called successfully.
-
-    // We encode a message with params is_internal: true/signer: signerNone because this is an internal message
-    // we would like to send. Then we will put the encoded internal message as an argument "payload" into the external
-    // message we will send to our multisig
-
-    const diceDeployMessage = (await client.abi.encode_message_body({
-      abi: diceContract.abi,
-      call_set: {
-        function_name: "constructor",
-        input: {},
-      },
-      is_internal: true,
-      signer: signerNone(),
-    })).body
-
-    const diceContractAddress = await diceContract.getAddress();
-
-    // We call submitTransaction method of multisig and pass diceDeployMessage and diceContractStateInit to deploy
-    // our dice contract.
-    // There we are creating an external message with arguments and sending it to the contract. For signing the message
-    // by default will be used "signer" which one we specified when created and "casinoOwnerMultisig" instance - casino_owner_keys
-    await casinoOwnerMultisig.run('submitTransaction', {
-      dest: diceContractAddress,
-      value: 9_000_000_000, // 9 evers
-      bounce: true,
-      allBalance: false,
-      payload: diceDeployMessage,
-      stateInit: diceContractStateInit
-    })
-
-    // SIMPLIFIED how submitTransaction of msig2 looks like:
-    // But if your msig2 has more than 1 custodian they will be putted into queue
-    // and will wait for confirmation from other custodians
-    //   function submitTransaction(
-    //     address dest,
-    //     uint128 value,
-    //     bool bounce,
-    //     bool allBalance,
-    //     TvmCell payload,
-    //     TvmCell stateInit
-    // ) {
-    //       require(msg.pubkey() == m_ownerKey, 100);
-    //       uint8 flags = FLAG_IGNORE_ERRORS(1) | FLAG_PAY_FWD_FEE_FROM_BALANCE(2);
-    //       if (allBalance) {
-    //           flags = FLAG_IGNORE_ERRORS(1) | FLAG_SEND_ALL_REMAINING(128);
-    //           value = 0;
-    //       }
-    //       dest.transfer({
-    //         value: value,
-    //         bounce: bounce,
-    //         flag: sendFlags,
-    //         body: payload,
-    //         stateInit: stateInit
-    //     });
-    //   }
-
-
-    console.log('Dice contract deployed, max bet is', nanoEversToEvers((await diceContract.runLocal('maxBet', {}, {})).decoded.output.value0));
-
-    // In the same way we create multisig for the player
-    let playerMultisig = new Account(SetcodeMultisigContract, {
-      signer: signerKeys(players_keys),
-      client,
-      initData: {},
-    });
-
-    const playerMultisigAddress = await casinoOwnerMultisig.getAddress();
-
-    await playerMultisig.deploy({
-      useGiver: true,
-      initInput: {
-        //constructor values
-        owners: [`0x${players_keys.public}`],
-        reqConfirms: 1,
-        lifetime: 3600
-      }})
-
-
-    // We encode a message with params is_internal: true/signer: signerNone because this is an internal message
-    // we would like to send. Then we will put the encoded internal message as an argument "payload" into the external
-    // message we will send to our multisig
-    const makeBetMessage = (await client.abi.encode_message_body({
-      abi: diceContract.abi,
-      call_set: {
-        function_name: "roll",
-        input: {
-          _bet_dice_value: 0
+const ever = new ProviderRpcClient({
+  fallback: () =>
+    EverscaleStandaloneClient.create({
+      connection: {
+        id: 1, // connection id
+        type: 'graphql',
+        group: "localnet",
+        data: {
+          endpoints: ['127.0.0.1'],
+          latencyDetectionInterval: 1000,
+          local: true,
         },
       },
-      is_internal: true,
-      signer: signerNone(),
-    })).body;
+      keystore: keyStore,
+      accountsStorage: accountStorage
+    }),
+});
 
-    for (let i = 0; i < 100; i++) {
-      // We use bignumber because it is good practise, maximum safe int in js is only 9_000_000 evers
-      // https://stackoverflow.com/questions/307179/what-is-javascripts-highest-integer-value-that-a-number-can-go-to-without-losin
+async function main() {
 
-      let maxBet = new BigNumber((await diceContract.runLocal('maxBet', {}, {})).decoded.output.value0);
-      if (maxBet.lt(0.6 * 1_000_000_000)) {
-        console.log('Dice contract has not enough balance to play');
-        break;
-      }
-      let ourBalance = new BigNumber(await playerMultisig.getBalance());
+  keyStore.addKeyPair(getGiverKeypair());
 
-      // 0.7 because 0.6 + gas;
-      if (ourBalance.lt(0.7 * 1_000_000_000)) {
-        console.log('We have not enough Evers to play');
-        break;
-      }
-      console.log('\nTry to roll a dice...');
+  // Generate a random keypair to set as owner of
+  // our SimpleStorage contract. In production, you
+  // of course need to import this keypair.
 
-      let result = await playerMultisig.run('sendTransaction', {
-        dest: diceContractAddress,
-        value: 600_000_000, // 0.6 evers
-        bounce: true, // Send funds back on any error in desctination account
-        flags: 1, // Pay delivery fee from the multisig balance, not from the value.
-        payload: makeBetMessage
-      });
+  // Player keypair
+  const playerKeys = SimpleKeystore.generateKeyPair();
+  keyStore.addKeyPair(playerKeys);
 
-      // Load list of all transactions and messages
-      let transaction_tree = await client.net.query_transaction_tree({
-        in_msg: result.transaction.in_msg,
-        abi_registry: [playerMultisig.abi, diceContract.abi]});
+  // Casino owner keypair
+  const diceOwnerKeys = SimpleKeystore.generateKeyPair();
+  keyStore.addKeyPair(diceOwnerKeys);
 
-      // Look for game event log
-      let gameLogMessage = transaction_tree.messages.find(m => m.src === diceContractAddress && m.decoded_body && m.decoded_body.name === 'Game');
-      if (!gameLogMessage)
-        throw new Error('Game not found');
+  // in-page-provider initially was designed as web3-like
+  // interface that one injected into web page from browser
+  // extension. So they have accounts(wallets) as first class
+  // citizen. In case we're using everscale-standalone-client
+  // we can manage accounts and keys on our side.
 
-      if (gameLogMessage.decoded_body.value.prize !== '0') {
-        console.log('We won', nanoEversToEvers(gameLogMessage.decoded_body.value.prize), 'Evers');
-        break;
-      } else {
-        console.log('Lose!')
-      }
-      await sleep(1);
+  // So in this example in additional for keyStore
+  // we are added simple accountStorage. So we can add
+  // 'wallet' smart contract as account and use
+  // them to start any transaction.
+
+  // EverWallet support several types of wallet,
+  // most popular - EverWallet and Multisig2.
+  // We will use ever wallet. It is very simple
+  // smart contract owned by a pubkey.
+
+  // It can be easily defined from pubkey.
+  // Address of the EverWallet smart-contract depend
+  // only on three params:
+  // 1. code
+  // 2. pubkey
+  // 3. nonce (by default undefinied, used to deploy several wallet from one pubkey)
+
+  // So we can define wallet just by pubkey, code is already
+  // hardcoded in the everscale-standalone-client.
+  const diceOwnerWallet = await EverWalletAccount.fromPubkey({publicKey: diceOwnerKeys.publicKey, workchain: 0});
+
+  // Send some tokens to our wallet
+  await getTokensFromGiver(ever, diceOwnerWallet.address, 10_000_000_000);
+
+  // Then just add our account to the account storage.
+  accountStorage.addAccount(diceOwnerWallet);
+
+  console.log('dice owner address is', diceOwnerWallet.address.toString());
+  // EverWallet no has a constructor and doesn't require
+  // separate action for deploy.
+  // in-page-provider will track is the wallet
+  // deployed and if it is not deployed provider will add
+  // the wallet's StateInit in the first external message.
+
+  // Now we will deploy DiceContract by internal message from our
+  // wallet. At first, we need to calculate dice contract address
+  // and stateInit
+
+  const {
+    address: diceExpectedAddress,
+    stateInit: diceExpectedStateInit
+  } = await ever.getStateInit(DiceContract.abi, {
+    // Dice code + empty data
+    tvc: DiceContract.tvc,
+    workchain: 0,
+    // we did not set pubkey, because our dice contract
+    // is internal owned (by internal messsage)
+    initParams: {
+      // static params only owner
+      owner_: diceOwnerWallet.address
+    }
+  });
+
+  const diceContract = new ever.Contract(DiceContract.abi, diceExpectedAddress);
+
+  // By using send({ from, amount }) we can send an internal message
+  // from the wallet account.
+  let tx = await extractError(diceContract.methods
+    .constructor({})
+    .send({
+      from: diceOwnerWallet.address,
+      amount: '7000000000', // 7 ever
+      stateInit: diceExpectedStateInit
+    }));
+
+  // This is just a sugar, what really happened in the
+  // lines above (pseudocode):
+  // Sdk encoded an internal message payload which one
+  // is calling constructor with no arguments
+  // This is the same as
+  // let payload = await (new diceContract.methods.constructor({}).encodeInternal();
+  // Then by specifying - from: diceOwnerWallet.address
+  // we tell provider to send this payload
+  // from the connected wallet account by internal
+  // and attach to this internal amount Ever's from the
+  // wallet account.
+  // Also we specified stateInit, and sdk will also add
+  // the stateInit to the internal message sended from the wallet account.
+
+  // EverWallet is written on low level language, but on solidity
+  // they probably will have function like this (pseudocode):
+
+  //   function submitTransaction(
+  //     address dest,
+  //     uint128 value,
+  //     bool bounce,
+  //     uint8 flags,
+  //     TvmCell payload,
+  //     optional(TvmCell) stateInit
+  // ) {
+  //       require(msg.pubkey() == tvm.pubkey(), 100);
+  //       dest.transfer({
+  //         value: value,
+  //         bounce: bounce,
+  //         flag: flags,
+  //         body: payload,
+  //         stateInit: stateInit
+  //     });
+  //   }
+
+  // So the all sugar in send: from - is just encoding target
+  // contract method call into internal message call and send
+  // from the connected wallet. Easy.
+
+  // One more important thing there, .send({from: address})
+  // return the first transaction in the transaction chain.
+
+  // So you have not any information is the internal message
+  // you sent from the wallet successful or not. Or ever
+  // you can not be sure is it delivered already or still
+  // on the way. Also, you can have a really long transaction chain.
+
+  // So to wait all transaction in the transaction tree you
+  // need to subscribe to the transaction tree and wait until
+  // all messages will be delivered.
+
+  let constructorCallSuccess = false;
+  const subscriber = new ever.Subscriber();
+  await subscriber.trace(tx).map(tx_in_tree => {
+    if (tx_in_tree.account._address === diceContract.address.toString() && tx_in_tree.aborted === false)
+      constructorCallSuccess = true;
+    return tx_in_tree;
+  }).finished();
+
+  if (!constructorCallSuccess) {
+    throw new Error(`Successful constructor transaction on ${diceContract.address.toString()} not found`);
+  }
+
+  console.log('dice contract deployed at', diceContract.address.toString());
+
+  // Okay, now we have dice contract deployed from diceOwnerWallet
+  // Let's deploy our player Wallet and try to play
+  const playerWallet = await EverWalletAccount.fromPubkey({publicKey: playerKeys.publicKey, workchain: 0});
+  await getTokensFromGiver(ever, playerWallet.address, 30_000_000_000);
+  accountStorage.addAccount(playerWallet);
+
+  console.log('player wallet address is', playerWallet.address.toString(), '\n');
+  // Let's play!
+  for (let i = 0; i < 20; i++) {
+    console.log('Try to play!');
+
+    let maxBet = (await diceContract.methods.maxBet({}).call()).value0;
+    if (new BigNumber(maxBet).lt(1_000_000_000)) {
+      throw new Error('Max bet is less then 1 ever');
     }
 
-    console.log("Test successful");
-  } catch (e) {
-    console.error(e);
+    // Call roll method of the diceContract by internal message from
+    // our player's wallet
+    let tx = await diceContract.methods.roll({_bet_dice_value: 5})
+      .send({
+        from: playerWallet.address,
+        amount: '1000000000', // 1 ever
+        stateInit: diceExpectedStateInit
+      });
+
+    // Looking for roll transaction
+    const subscriber = new ever.Subscriber();
+    let play_tx = await subscriber.trace(tx).filter(tx_in_tree => {
+      return tx_in_tree.account._address === diceContract.address.toString();
+    }).first();
+
+    if (!play_tx) {
+      throw new Error('Play transaction is not found!');
+    }
+
+    // Looking for the event Game(address player, uint8 bet, uint8 result,  uint128 prize);
+    let decoded_events = await diceContract.decodeTransactionEvents({
+      transaction: play_tx,
+    });
+
+    if (decoded_events[0].data.bet === decoded_events[0].data.result) {
+      console.log('We won', new BigNumber(decoded_events[0].data.prize).shiftedBy(-9).toFixed(1), 'Evers\n');
+      break;
+    } else {
+      console.log('We lose\n');
+    }
+    await sleep(1);
   }
+
+  console.log("Test successful");
 }
 
 (async () => {
   try {
     console.log("Hello localhost EVER!");
-    await main(client);
+    await main();
     process.exit(0);
   } catch (error) {
-    if (error.code === 504) {
-      console.error(`Network is inaccessible. You have to start TON OS SE using \`tondev se start\`.\n If you run SE on another port or ip, replace http://localhost endpoint with http://localhost:port or http://ip:port in index.js file.`);
-    } else {
-      console.error(error);
-    }
+    console.error(error);
   }
-  client.close();
 })();
 
-// To human readable amount
-function nanoEversToEvers(nano) {
-  return (nano / 1_000_000_000).toString();
+async function extractError(transactionPromise) {
+  return transactionPromise.then(res => {
+    if (res.transaction?.aborted || res.aborted) {
+      throw new Error(`Transaction aborted with code ${res.transaction?.exitCode || res.exitCode}`)
+    }
+    return res;
+  });
 }
 
 function sleep(seconds) {
